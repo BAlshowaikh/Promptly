d529b99dc05a715b11dfc465aa262257b15d3faf
BAlshowaikh <batoolalshowaikh04@outlook.com>
2026-02-09 19:49:42 +0300

    Register all apps' models in admin

--- apps/developer/admin.py
+++ apps/developer/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import ( 
+    DevSession, 
+    DevRunResult,
+    DevRun,
+    DevSessionModelConfig
+)
+
+
+admin.site.register(DevSession)
+admin.site.register(DevRunResult)
+admin.site.register(DevRun)
+admin.site.register(DevSessionModelConfig)
